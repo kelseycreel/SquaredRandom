7d38702e5af76f556ead9f9a14469c58755242c6
Kelsey Creel <kelseycreel@gmail.com>
2020-03-18 18:50:10 -0500

    square

--- SquaredRandom/Program.cs
+++ SquaredRandom/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SquaredRandom
 {
@@ -10,15 +11,26 @@ namespace SquaredRandom
             Random rand = new Random();
             var randNums = new List<int>();
 
-            for (int count = 1; count <= 20; count++)
+            for (int count = 0; count < 20; count++)
                  randNums.Add(rand.Next(1, 51));
 
             var squaredNums = new List<int>();
-            foreach (var num in randNums)
-            {
-                var newNum = num * num;
-                squaredNums.Add(newNum);
-            }
+            //foreach (var num in randNums)
+            //{
+            //    var newNum = num * num;
+            //    squaredNums.Add(newNum);
+            //}
+
+            squaredNums = randNums.Select(x => x * x).ToList();
+
+            var squaredNumNoOdds = new List<int>();
+            //foreach (var num in squaredNums)
+            //{
+            //    if (num % 2 == 0)
+            //        squaredNumNoOdds.Add(num);
+            //}
+            squaredNumNoOdds = squaredNums.Select(x => x % 2).ToList();
+
         }
     }
 }
