35aaca1783dcad3e9eda93652418a8bb0b3eb067
Kelsey Creel <kelseycreel@gmail.com>
2020-03-18 18:18:03 -0500

    Add project files.

---
+++ SquaredRandom/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SquaredRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            var randNums = new List<int>();
+
+            for (int count = 1; count <= 20; count++)
+                 randNums.Add(rand.Next(1, 51));
+
+            var squaredNums = new List<int>();
+            foreach (var num in randNums)
+            {
+                var newNum = num * num;
+                squaredNums.Add(newNum);
+            }
+        }
+    }
+}
